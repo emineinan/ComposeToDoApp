8de2656e9b75852a58112d0cfc2c71bd35016974
Emine INAN <emineinanx@gmail.com>
2023-03-03 13:37:53 +0300

    Update Constants.kt

--- app/src/main/java/com/example/composetodoapp/util/Constants.kt
+++ app/src/main/java/com/example/composetodoapp/util/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
     const val DATABASE_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_database"
 
+    const val SPLASH_SCREEN = "splash"
     const val LIST_SCREEN = "list/{action}"
     const val TASK_SCREEN = "task/{taskId}"
 
@@ -11,6 +12,7 @@ object Constants {
     const val TASK_ARGUMENT_KEY = "taskId"
 
     const val MAX_TITLE_LENGTH = 20
+    const val SPLASH_SCREEN_DELAY = 3000L
 
     const val PREFERENCE_NAME = "todo_preferences"
     const val PREFERENCE_KEY = "sort_state"
